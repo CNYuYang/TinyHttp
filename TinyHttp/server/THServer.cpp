514e90d4882c755c40abb1cee36f412a9bad5701
余为民同志 <42082796+CNYuYang@users.noreply.github.com>
2021-09-03 17:30:24 +0800

    Update THServer.cpp

--- TinyHttp/server/THServer.cpp
+++ TinyHttp/server/THServer.cpp
@@ -1,104 +1,105 @@
-#include "THServer.h"
-#include "THGlobal.h"
-#include <cstdio>
+#ifndef TINYHTTP_THSERVER_H
+#define TINYHTTP_THSERVER_H
+
+#include "CommScheduler.h"
+#include "Communicator.h"
+#include "THTask.h"
+#include <atomic>
+#include <functional>
 #include <mutex>
-#include <netdb.h>
-#include <openssl/ssl.h>
+#include <sys/socket.h>
+#include <sys/types.h>
 
-#define PORT_STR_MAX 5
+struct THServerParams {
+    int peer_response_timeout; /*每次读写操作的超时时间*/
+    int receive_timeout;       /*接收到整个信息的超时时间*/
+};
 
-int THServerBase::init(const struct sockaddr *bind_addr, socklen_t addrlen, const char *cert_file, const char *key_file) {
+static constexpr struct THServerParams SERVER_PARAMS_DEFAULT = {
+        .peer_response_timeout = 10 * 1000,
+        .receive_timeout = -1};
 
-    /*超时时间选择，如果同时配置了peer_response_timeout/receive_timeout 选择其中较小的那个 */
+class THServerBase : protected CommService {
 
-    int timeout = this->params.peer_response_timeout;
-
-    if (this->params.receive_timeout >= 0) {
-        if ((unsigned int) timeout > (unsigned int) this->params.receive_timeout) {
-            timeout = this->params.receive_timeout;
-        }
+public:
+    THServerBase(const struct THServerParams *params) : conn_count(0) {
+        this->params = *params;
+        //this->listen_fd = -1;
+        //this->unbind_finish = false;
     }
 
-    //调用父类 @Link{Communicator.cpp} 的init()方法
-    if (this->CommService::init(bind_addr, addrlen, -1, timeout) < 0)
-        return -1;
-
-    //设置ssl证书
-    if (key_file && cert_file) {
-        if (this->init_ssl_ctx(cert_file, key_file) < 0) {
-            this->deinit();
-            return -1;
-        }
+public:
+    /* 监听 IPv4 的端口. */
+    int start(unsigned short port) {
+        return start(AF_INET, NULL, port, NULL, NULL);
     }
 
-    this->scheduler = THGlobal::get_scheduler();
-    return 0;
-}
+    /* 选择IP协议 IPV4:AF_INET IPV6:AF_INET6 */
+    int start(int family, unsigned short port) {
+        return start(family, NULL, port, NULL, NULL);
+    }
 
-/*暂时不配置ssl信息*/
-// @TODO ssl相关
-int THServerBase::init_ssl_ctx(const char *cert_file, const char *key_file) {
-    return 0;
+    /* 用hostname和端口号开启一个IPV4服务 */
+    int start(const char *host, unsigned short port) {
+        return start(AF_INET, host, port, NULL, NULL);
+    }
+
+    /* 用IP协议、hostname和端口号开启一个IPV4服务*/
+    int start(int family, const char *host, unsigned short port) {
+        return start(family, host, port, NULL, NULL);
+    }
+
+
+    int start(int family, const char *host, unsigned short port,
+              const char *cert_file, const char *key_file);
+
+    /* 真正的启动函数 */
+    int start(const struct sockaddr *bind_addr, socklen_t addrlen,
+              const char *cert_file, const char *key_file);
+
+    /* stop() 是一个阻塞操作 */
+    void stop() {
+        this->shutdown();
+        this->wait_finish();
+    };
+
+    void shutdown();
+    void wait_finish();
+
+public:
+    size_t get_conn_count() const { return this->conn_count; }
+
+protected:
+    THServerParams params;
+
+private:
+    int init_ssl_ctx(const char *cert_file, const char *key_file);
+    int init(const struct sockaddr *bind_addr, socklen_t addrlen, const char *cert_file, const char *key_file);
+
+protected:
+    std::atomic<size_t> conn_count;
+
+private:
+    std::mutex mutex;
+    CommScheduler *scheduler;
+};
+
+//@TODO 未开始
+template<class REQ, class RESP>
+class THServer : public THServerBase {
+public:
+    THServer(const struct THServerParams *params,
+             std::function<void(THNetworkTask<REQ, RESP> *)> proc) : THServerBase(params),
+                                                                     process(std::move(proc)) {
+    }
+
+    THServer(std::function<void(THNetworkTask<REQ, RESP> *)> proc) : THServerBase(&SERVER_PARAMS_DEFAULT),
+                                                                     process(std::move(proc)) {
+    }
+
+protected:
+    std::function<void(THNetworkTask<REQ, RESP> *)> process;
 };
 
 
-int THServerBase::start(int family, const char *host, unsigned short port,
-                        const char *cert_file, const char *key_file) {
-    struct addrinfo hints = {
-            .ai_flags = AI_PASSIVE,
-            .ai_family = family,
-            .ai_socktype = SOCK_STREAM,
-    };
-
-    struct addrinfo *addrinfo;
-    char port_str[PORT_STR_MAX + 1];
-    int ret;
-    snprintf(port_str, PORT_STR_MAX + 1, "%d", port);
-
-    ret = getaddrinfo(host, port_str, &hints, &addrinfo);
-
-    //获取成功
-    if (ret == 0) {
-        ret = start(addrinfo->ai_addr, (socklen_t) addrinfo->ai_addrlen,
-                    cert_file, key_file);
-        freeaddrinfo(addrinfo);
-    }
-
-    else {
-        if (ret != EAI_SYSTEM)
-            errno = EINVAL;
-        ret = -1;
-    }
-
-    return ret;
-}
-
-int THServerBase::start(const struct sockaddr *bind_addr, socklen_t addrlen, const char *cert_file,
-                        const char *key_file) {
-    SSL_CTX *ssl_ctx;
-    // @TODO ssl相关
-    if (this->init(bind_addr, addrlen, cert_file, key_file) >= 0) {
-
-        //绑定成功
-        if (this->scheduler->bind(this) >= 0)
-            return 0;
-
-        //失败释放资源
-        ssl_ctx = this->get_ssl_ctx();
-        this->deinit();
-        if (ssl_ctx)
-            SSL_CTX_free(ssl_ctx);
-    }
-
-    return -1;
-}
-
-void THServerBase::shutdown() {
-    this->scheduler->unbind(this);
-}
-
-void THServerBase::wait_finish() {
-    SSL_CTX *ssl_ctx = this->get_ssl_ctx();
-    std::unique_lock<std::mutex> lock(this->mutex);
-
-}
+#endif//TINYHTTP_THSERVER_H
